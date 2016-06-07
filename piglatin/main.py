a72b0ae80ff4761ee9c085d0a2f25cc156818da8
scosares <scosares@lagcc.cuny.edu>
2016-06-07 17:34:07 -0400

    Add files via upload

---
+++ piglatin/main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+
+
+class ProgramHandler(webapp2.RequestHandler):
+    def get(self):
+        tot=10
+        self.response.write(tot)
+
+class PigHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Stop being such a pig!')
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+    ('/program', ProgramHandler),
+    ('/pig', PigHandler)
+], debug=True)
